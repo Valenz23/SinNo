4f2649c48e93506c0347406f0315c32726675db7
Pablo Valenzuela <valenbetis@gmail.com>
2023-11-15 12:44:48 +0100

    Update test_liked_list.py

--- src/tests/test_liked_list.py
+++ src/tests/test_liked_list.py
@@ -2,25 +2,25 @@ import unittest
 import time
 
 class Testing(unittest.TestCase):
-    def test_creating_liked_list(self):
+    def test_0_creating_liked_list(self):
         a = 'some'
         b = 'some'
         time.sleep(0.7)
         self.assertEqual(a, b)
 
-    def test_adding_songs_to_liked_list(self):
+    def test_1_adding_songs_to_liked_list(self):
         a = 'some'
         b = 'some'
-        time.sleep(1)
+        time.sleep(1.5)
         self.assertEqual(a, b)
 
-    def test_removing_songs_to_liked_list(self):
+    def test_2_removing_songs_to_liked_list(self):
         a = 'some'
         b = 'some'
         time.sleep(0.5)
         self.assertEqual(a, b)
 
-    def test_removing_liked_list(self):
+    def test_3_removing_liked_list(self):
         a = 'some'
         b = 'some'
         time.sleep(0.3)
