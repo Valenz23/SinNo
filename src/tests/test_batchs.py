60c7a3cfb87de23af800525b61446144a0dddb88
Pablo Valenzuela <valenbetis@gmail.com>
2023-11-15 12:46:17 +0100

    Update test_batchs.py

--- src/tests/test_batchs.py
+++ src/tests/test_batchs.py
@@ -2,16 +2,16 @@ import unittest
 import time
 
 class Testing(unittest.TestCase):
-    def test_adding_batch_songs(self):
+    def test_0_adding_batch_songs(self):
         a = 'some'
         b = 'some'
-        time.sleep(5)
+        time.sleep(4.5)
         self.assertEqual(a, b)
 
-    def test_removing_batch_songs(self):
+    def test_1_removing_batch_songs(self):
         a = 'some'
         b = 'some'
-        time.sleep(5)
+        time.sleep(3.8)
         self.assertEqual(a, b)
 
 if __name__ == '__main__':
