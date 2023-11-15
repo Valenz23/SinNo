87c9a150188e19a8adb641c4a83153c4c46b728b
Pablo Valenzuela <valenbetis@gmail.com>
2023-11-15 12:45:45 +0100

    Update test_users.py

--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -2,22 +2,22 @@ import unittest
 import time
 
 class Testing(unittest.TestCase):
-    def test_creating_users(self):
+    def test_0_creating_users(self):
         a = 'some'
         b = 'some'
         time.sleep(0.5)
         self.assertEqual(a, b)
 
-    def test_removing_users(self):
-        a = 'some'
-        b = 'some'
-        time.sleep(0.3)
-        self.assertEqual(a, b)
-
-    def test_modifying_users(self):
+    def test_1_modifying_users(self):
         a = 'some'
         b = 'some'
         time.sleep(0.7)
+        self.assertEqual(a, b)
+
+    def test_2_removing_users(self):
+        a = 'some'
+        b = 'some'
+        time.sleep(0.3)
         self.assertEqual(a, b)
 
 if __name__ == '__main__':
