fc51dc23864abc984d6a250ca7c706ef8c90ffa7
Pablo Valenzuela <valenbetis@gmail.com>
2023-11-15 12:44:14 +0100

    Update test_songs.py

--- src/tests/test_songs.py
+++ src/tests/test_songs.py
@@ -2,22 +2,22 @@ import unittest
 import time
 
 class Testing(unittest.TestCase):
-    def test_creating_songs(self):
+    def test_0_creating_songs(self):
         a = 'some'
         b = 'some'
         time.sleep(0.5)
         self.assertEqual(a, b)
 
-    def test_removing_songs(self):
-        a = 'some'
-        b = 'some'
-        time.sleep(0.3)
-        self.assertEqual(a, b)
-
-    def test_modifying_songs(self):
+    def test_1_modifying_songs(self):
         a = 'some'
         b = 'some'        
         time.sleep(1)
+        self.assertEqual(a, b)
+
+    def test_2_removing_songs(self):
+        a = 'some'
+        b = 'some'
+        time.sleep(0.3)
         self.assertEqual(a, b)
 
 if __name__ == '__main__':
