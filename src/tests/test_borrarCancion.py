97331dd2355cff6600271155a0cfed1f87e148f1
Valenz23 <valenbetis@gmail.com>
2023-12-18 12:41:54 +0100

    close #7

--- src/tests/test_borrarCancion.py
+++ src/tests/test_borrarCancion.py
@@ -4,7 +4,7 @@ import unittest
 class Testing(unittest.TestCase):
     def test_borrar_cancion(self):
 
-        # borraamos la canción de shakira del otro test
+        # borraaamos la canción de shakira del otro test
 
         a = admin()
 
