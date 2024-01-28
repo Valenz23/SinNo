641ac45ca6a4eb18abfbc07e41c6dac006bdf794
Valenz23 <valenbetis@gmail.com>
2024-01-28 14:36:40 +0100

    test

--- src/tests/test_listarCancion.py
+++ src/tests/test_listarCancion.py
@@ -2,7 +2,7 @@ from clases.user import *
 import unittest
 
 class Testing(unittest.TestCase):
-    def test_listar_canciones(self):        
+    def test_listar_canciones(self):         
         lista = listarCanciones()
         self.assertTrue(not lista.empty)
 
